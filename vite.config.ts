92fc86b9e251998c42aaeafc020c751205681f3c
王乾润 <2015104036@stu.hqu.edu.cn>
2023-04-16 22:48:45 +0800

    生产环境移除console

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
